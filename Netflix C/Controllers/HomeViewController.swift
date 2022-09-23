8ece38b23ed15c863e077b33e92027ff8dcb6f4e
Raidansz <92235772+Raidansz@users.noreply.github.com>
2022-09-23 22:35:27 +0200

    Setting labels for the sections

--- Netflix C/Controllers/HomeViewController.swift
+++ Netflix C/Controllers/HomeViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    
+     let sectionsTitles: [String] = ["Trending Movies","Popular", "Trending Tv","Upcoming Movies","Top Rated"]
+    
+    
+    
+    
 
     private let homeFeedTable: UITableView = {
         let table = UITableView(frame: .zero,style: .grouped)
@@ -72,7 +79,7 @@ class HomeViewController: UIViewController {
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 20
+        return sectionsTitles.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
@@ -102,6 +109,20 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
     }
+    
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else {return}
+        header.textLabel?.font = .systemFont(ofSize: 18,weight: .semibold)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x+20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionsTitles[section]
+    }
+    
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let defaultOffset = view.safeAreaInsets.top
