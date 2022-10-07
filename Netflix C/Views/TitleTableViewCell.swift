377c6fbd50db6c036c8ee647d18c2665f7f16176
Raidansz <92235772+Raidansz@users.noreply.github.com>
2022-10-07 21:21:10 +0200

    Applying constraints to the upcoming controller

--- Netflix C/Views/TitleTableViewCell.swift
+++ Netflix C/Views/TitleTableViewCell.swift
@@ -11,6 +11,12 @@ class TitleTableViewCell: UITableViewCell {
 
     static let identifier = "TitleTableViewCell"
     
+    private let playTitleButton:UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+    }()
+    
+    
     private let titleLabel:UILabel = {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -27,7 +33,27 @@ class TitleTableViewCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(titlesPosterUIImageView)
         contentView.addSubview(titleLabel)
+        contentView.addSubview(playTitleButton)
+        
+        applyConstraints()
+
     }
+    
+    
+    private func applyConstraints(){
+        let titlesPosterUIImageViewConstraints = [
+            titlesPosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titlesPosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor,constant: 15)
+            titlesPosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor,constant: -15)
+            titlesPosterUIImageView.widthAnchor.constraint(equalToConstant:100)
+             
+        ]
+        NSLayoutConstraint.activate(titlesPosterUIImageViewConstraints)
+    }
+    
+    
+    
+    
     required  init?(coder: NSCoder) {
         fatalError()
     }
