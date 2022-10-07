466163fdf197ac4691dbb46a0dcb7452980d2da0
Raidansz <92235772+Raidansz@users.noreply.github.com>
2022-10-07 22:02:59 +0200

    Setting a title for the search controller

--- Netflix C/Controllers/SearchViewController.swift
+++ Netflix C/Controllers/SearchViewController.swift
@@ -12,6 +12,13 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        
+        
+        
         view.backgroundColor = .systemBackground
     }
     
