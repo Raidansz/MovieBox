286bf873ce39a8c897794239da07263400ac3a51
Raidansz <92235772+Raidansz@users.noreply.github.com>
2022-10-24 15:39:48 +0200

    Refactoring TableViewHeader Hero title

--- Netflix C/Controllers/Core/HomeViewController.swift
+++ Netflix C/Controllers/Core/HomeViewController.swift
@@ -19,7 +19,8 @@ enum Sections: Int {
 
 class HomeViewController: UIViewController {
     
-    
+    private var randomTrendingMovie: Title?
+    private var headerView: HeroHeaderUIView?
      let sectionsTitles: [String] = ["Trending Movies","Trending Tv","Popular", "Upcoming Movies","Top Rated"]
     
     
@@ -48,13 +49,28 @@ class HomeViewController: UIViewController {
         configureNavbar()
         
         
-        let headerView = HeroHeaderUIView(frame: CGRect(x:0,y:0,width: view.bounds.width, height: 500))
+         headerView = HeroHeaderUIView(frame: CGRect(x:0,y:0,width: view.bounds.width, height: 500))
         homeFeedTable.tableHeaderView = headerView
-        
-        
-  
+        configureHeroHeaderView()
         
     }
+    
+    private func configureHeroHeaderView(){
+        
+        APICaller.shared.getTrendingMovies { [weak self] result in
+            switch result{
+            case .success(let titles):
+                let selectedTitle = titles.randomElement()
+                self?.randomTrendingMovie = selectedTitle
+                self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    
+    
     private func configureNavbar(){
        // var image = UIImage(named: "netflixLogo")
        // image = image?.withRenderingMode(.alwaysOriginal)
--- Netflix C/Views/HeroHeaderUIView.swift
+++ Netflix C/Views/HeroHeaderUIView.swift
@@ -83,6 +83,13 @@ class HeroHeaderUIView: UIView {
         NSLayoutConstraint.activate(downloadButtonConstraints)
     }
     
+    public func configure(with model: TitleViewModel){
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500/\(model.posterURL)")else{
+                   return
+               }
+        
+        heroImageview.sd_setImage(with: url,completed: nil)
+    }
     
     override func layoutSubviews() {
         super.layoutSubviews()
