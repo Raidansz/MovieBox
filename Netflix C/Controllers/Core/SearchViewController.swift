3451cfd4489abf64f8a40df5944260c468bb9d8c
Raidansz <92235772+Raidansz@users.noreply.github.com>
2022-10-24 16:17:19 +0200

    Handling Tapping across all ViewControllers

--- Netflix C/Controllers/Core/SearchViewController.swift
+++ Netflix C/Controllers/Core/SearchViewController.swift
@@ -135,7 +135,7 @@ extension SearchViewController: UITableViewDelegate,UITableViewDataSource {
 }
 
 
-extension SearchViewController:UISearchResultsUpdating{
+extension SearchViewController:UISearchResultsUpdating, SearchResultsViewControllerDelegate{
     
     func updateSearchResults(for searchController: UISearchController) {
         let searchBar = searchController.searchBar
@@ -145,7 +145,7 @@ extension SearchViewController:UISearchResultsUpdating{
               let resultsController = searchController.searchResultsController as? SearchResultsViewController else {
             return
         }
-        
+        resultsController.delegate = self
         APICaller.shared.search(with: query){ results in
             DispatchQueue.main.async {
                 switch results{
@@ -161,5 +161,14 @@ extension SearchViewController:UISearchResultsUpdating{
         
         
         
+    }
+    func searchResultsViewControllerDidTapItem(_ viewModel: TitlePreviewViewModel) {
+        
+        DispatchQueue.main.async { [weak self] in
+            let vc = TitlePreviewViewController()
+            vc.configure(with: viewModel)
+            self?.navigationController?.pushViewController(vc, animated: true)
+        }
+       
     }
 }
